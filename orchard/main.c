a17db642b36b0eb8abd3a547f796f1ef42bf06e0
bunnie <bunnie@kosagi.com>
2015-12-19 02:38:37 +0800

    add Orchard-specific files

---
+++ orchard/main.c
@@ -0,0 +1,139 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.h"
+#include "hal.h"
+#include "i2c.h"
+#include "spi.h"
+
+#include "shell.h"
+#include "chprintf.h"
+
+#include "orchard.h"
+#include "orchard-shell.h"
+#include "orchard-events.h"
+
+#include <string.h>
+
+struct evt_table orchard_events;
+
+static const I2CConfig i2c_config = {
+  100000
+};
+
+#if 0 ////// TODO: SPI needs to be refactored to work with the 8-bit implementation here (16 bit from reference code)
+// for now disabling in mcuconf.h and halconf.h
+static const SPIConfig spi_config = {
+  NULL,
+  GPIOC,
+  4, 
+  KINETIS_SPI_TAR_SYSCLK_DIV_8(8)
+};
+#endif
+
+static void shell_termination_handler(eventid_t id) {
+  static int i = 1;
+  (void)id;
+
+  chprintf(stream, "\r\nRespawning shell (shell #%d, event %d)\r\n", ++i, id);
+  orchardShellRestart();
+}
+
+
+extern int print_hex(BaseSequentialStream *chp,
+                     const void *block, int count, uint32_t start);
+
+static void print_mcu_info(void) {
+  uint32_t sdid = SIM->SDID;
+  const char *famid[] = {
+    "KL0%d (low-end)",
+    "KL1%d (basic)",
+    "KL2%d (USB)",
+    "KL3%d (Segment LCD)",
+    "KL4%d (USB and Segment LCD)",
+  };
+  const uint8_t ram[] = {
+    0,
+    1,
+    2,
+    4,
+    8,
+    16,
+    32,
+    64,
+  };
+
+  const uint8_t pins[] = {
+    16,
+    24,
+    32,
+    36,
+    48,
+    64,
+    80,
+  };
+
+  if (((sdid >> 20) & 15) != 1) {
+    chprintf(stream, "Device is not Kinetis KL-series\r\n");
+    return;
+  }
+
+  chprintf(stream, famid[(sdid >> 28) & 15], (sdid >> 24) & 15);
+  chprintf(stream, " with %d kB of ram detected"
+                   " (Rev: %04x  Die: %04x  Pins: %d).\r\n",
+                   ram[(sdid >> 16) & 15],
+                   (sdid >> 12) & 15,
+                   (sdid >> 7) & 31,
+                   pins[(sdid >> 0) & 15]);
+}
+
+/*
+ * Application entry point.
+ */
+int main(void)
+{
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+#if 0
+  spiStart(&SPID1, &spi_config); 
+  spiObjectInit(&SPID1);
+#endif
+
+  evtTableInit(orchard_events, 32);
+
+  orchardShellInit();
+
+  orchardEventsStart();
+
+  evtTableHook(orchard_events, shell_terminated, shell_termination_handler);
+
+  chprintf(stream, "\r\n\r\nOrchard shell.  Based on build %s\r\n", gitversion);
+  print_mcu_info();
+
+  orchardShellRestart();
+
+  while (TRUE)
+    chEvtDispatch(evtHandlers(orchard_events), chEvtWaitOne(ALL_EVENTS));
+}
+
